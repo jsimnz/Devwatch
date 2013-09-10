f6e4a42d12204a4c16339422c6cecf3dc8acc4ac
John-Alan Simmons <simmons.johnalan@gmail.com>
2013-09-10 19:36:21 -0400

    Completed JS script and example

--- devwatch.go
+++ devwatch.go
@@ -1,3 +1,34 @@
+/*
+The BSD 3-Clause
+
+Copyright (c) 2013 John-Alan Simmons
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+  Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+  Neither the name of the {organization} nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import (
@@ -5,6 +36,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/howeyc/fsnotify"
+	"log"
 	"net/http"
 	"os"
 )
@@ -43,7 +75,7 @@ func (h *hub) run() {
 			delete(h.connections, c)
 			close(c.send)
 		case m := <-h.broadcast:
-			fmt.Printf("Broadcasting: %s\n", m)
+			log.Printf("Broadcasting: %s\n", m)
 			for c := range h.connections {
 				select {
 				case c.send <- m:
@@ -85,7 +117,7 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Getting current working directory...")
+	log.Println("Getting current working directory...")
 	pwd, err := os.Getwd()
 	if err != nil {
 		log.Panic(err)
@@ -99,7 +131,7 @@ func main() {
 	go h.run()
 	go watchNRefresh(pwd)
 
-	log.Println("Starting server at localhost:%s\n", port)
+	log.Printf("Starting server at localhost:%s\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
